a559215452378233f761baf1e7069d370a0ea193
Nazar <nazarpas2007@gmail.com>
2024-10-15 00:55:46 +0300

    add function to find transitive closure of matrix (complete task 3)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """ Module to work with relations and their matrixes """
+from copy import deepcopy
 
 def read_matrix(file_name: str) -> tuple[list[list[int]], int]:
     """Read matrix from file and return it with its dimension.
@@ -33,8 +34,8 @@ def save_matrix(file_name: str, matrix: list[list[int]]) -> None:
         text = ''
         for line in matrix:
             row = ''
-            for el in line:
-                row += str(el)
+            for el_ in line:
+                row += str(el_)
             text += row + '\n'
         file.write(text[:-1])
 
@@ -86,6 +87,40 @@ def relation_to_matrix(set_of_elements: list[object],
             matrix[-1].append( int((el_a, el_b) in relation) )
 
     return matrix
+
+def matrix_transitive_closure(matrix: list[list[int]]) -> list[list[int]]:
+    """ Return transitive matrix closure,
+    use Warshall’s algorithm to find one
+
+    Args:
+        matrix (list[list[int]]): original matrix
+
+    Returns:
+        list[list[int]]: transitive closure of matrix
+
+    Examples:
+    >>> matrix_transitive_closure([[0, 1, 0, 1], [1, 0, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0]])
+    [[1, 1, 0, 1], [1, 1, 0, 1], [0, 0, 1, 0], [1, 1, 0, 1]]
+    """
+    matrix_size = len(matrix)
+
+    current_matrix = deepcopy(matrix)
+    old_matrix = []
+
+    # Dont automatically copy i'th row.
+    # This case is being handle by normal row processing.
+    for i in range(matrix_size):
+        old_matrix = deepcopy(current_matrix)
+        for y, row in enumerate(old_matrix):
+            # if i'th element of row is 0, copy it to current_matrix
+            if row[i] == 0:
+                # This row has already been copied in deepcopy()
+                continue
+
+            for x, el_ in enumerate(row):
+                current_matrix[y][x] = el_ or (old_matrix[y][i] and old_matrix[i][x])
+
+    return current_matrix
 
 if __name__ == "__main__":
     import doctest
